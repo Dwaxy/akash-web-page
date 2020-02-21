d45bb62a28e838f534b811d7d38c603ef865a8d4
akash-yoobee <54612932+akash-yoobee@users.noreply.github.com>
2020-02-21 18:46:06 +1300

    First commit friday akash

---
+++ src/components/akash/about-akash.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <h1>Some info about akash</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about-akash"
+};
+</script>
+
+<style scoped>
+</style>
